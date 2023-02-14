8127f171ee127a114f19a257b4529b34a9581b28
JamiuKolawole04 <Jakko4real@gmail.com>
2023-02-14 18:45:30 +0100

    chore: adding skeleton props

--- src/components/CardSkeleton.js
+++ src/components/CardSkeleton.js
@@ -1,5 +1,15 @@
-import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 export const CardSkeleton = () => {
-  return <div>CardSkeleton</div>;
+  return (
+    <div className="card-skeleton">
+      <div className="left-col">
+        <Skeleton circle direction="rtl" height={50} width={50} />
+      </div>
+
+      <div className="right-col">
+        <Skeleton />
+      </div>
+    </div>
+  );
 };
